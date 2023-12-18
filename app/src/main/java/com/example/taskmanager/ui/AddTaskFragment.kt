037a9b06619c0b8c9667aa8e05e9085552365164
HaoDang181 <95413604+HaoDang181@users.noreply.github.com>
2023-12-18 10:54:23 +0700

    bla bla bla

--- app/src/main/java/com/example/taskmanager/ui/AddTaskFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/AddTaskFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
 import com.example.taskmanager.R
 
 class AddTaskFragment : Fragment() {
@@ -17,11 +15,6 @@ class AddTaskFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_task, container, false)
-        val button = view.findViewById<Button>(R.id.navigate2)
-        button.setOnClickListener{
-            findNavController().navigate(R.id.action_addTaskFragment_to_homeFragment)
-        }
         return view
     }
-
 }
